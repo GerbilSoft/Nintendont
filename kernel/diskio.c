GerbilSoft/Nintendont

commit 11840497ee5b32c219fc6f1c2cbdf6d775adeb0f

[kernel] diskio.c: Partially rebased on FatFS R0.11a.
Nintendont currently uses separate functions for SD and USB.
FatFS does have an option for selecting a physical device number,
which could be used instead of the separate function pointers,
but it isn't at the moment.

TODO: Implement get_fattime().

kernel/diskio.c
@@ -1,3 +1,12 @@
+/*-----------------------------------------------------------------------*/
+/* Low level disk I/O module skeleton for FatFs     (C)ChaN, 2014        */
+/*-----------------------------------------------------------------------*/
+/* If a working storage control module is available, it should be        */
+/* attached to the FatFs via a glue function rather than modifying it.   */
+/* This is an example of glue functions to attach various exsisting      */
+/* storage control modules to the FatFs module with a defined API.       */
+/*-----------------------------------------------------------------------*/
+
 #include "diskio.h"
 #include "string.h"
 #include "debug.h"
@@ -8,10 +17,45 @@
 #include "alloc.h"
 #include "common.h"
 
-u32 s_size;
-u32 s_cnt;
+u32 s_size;	// Sector size.
+u32 s_cnt;	// Sector count.
 
-DRESULT disk_read_sd( BYTE drv, BYTE *buff, DWORD sector, BYTE count )
+
+/*-----------------------------------------------------------------------*/
+/* Get Drive Status                                                      */
+/*-----------------------------------------------------------------------*/
+DSTATUS disk_status (
+	BYTE pdrv		/* Physical drive nmuber to identify the drive */
+)
+{
+	return RES_OK;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Inidialize a Drive                                                    */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_initialize (
+	BYTE pdrv				/* Physical drive nmuber to identify the drive */
+)
+{
+	/* Nintendont initializes devices outside of FatFS. */
+	return RES_OK;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Read Sector(s) [SD card]                                              */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_read_sd (
+	BYTE pdrv,		/* Physical drive nmuber to identify the drive */
+	BYTE *buff,		/* Data buffer to store read data */
+	DWORD sector,	/* Sector address in LBA */
+	UINT count		/* Number of sectors to read */
 {
 	s32 Retry=10;
 
@@ -37,16 +81,39 @@ DRESULT disk_read_sd( BYTE drv, BYTE *buff, DWORD sector, BYTE count )
 
 	return RES_OK;
 }
-// Write Sector(s)
-DRESULT disk_write_sd( BYTE drv, const BYTE *buff, DWORD sector, BYTE count )
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Write Sector(s) [SD card]                                             */
+/*-----------------------------------------------------------------------*/
+
+#if _USE_WRITE
+DRESULT disk_write_sd (
+	BYTE pdrv,			/* Physical drive nmuber to identify the drive */
+	const BYTE *buff,	/* Data to be written */
+	DWORD sector,		/* Sector address in LBA */
+	UINT count			/* Number of sectors to write */
+)
 {
 	if( sdio_WriteSectors( sector, count, buff ) < 0 )
 		return RES_ERROR;
 
 	return RES_OK;
 }
+#endif
 
-DRESULT disk_read_usb(BYTE drv, BYTE *buff, DWORD sector, BYTE count)
+
+
+/*-----------------------------------------------------------------------*/
+/* Read Sector(s) [USB storage]                                          */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_read_usb (
+	BYTE pdrv,		/* Physical drive nmuber to identify the drive */
+	BYTE *buff,		/* Data buffer to store read data */
+	DWORD sector,	/* Sector address in LBA */
+	UINT count		/* Number of sectors to read */
 {
 	if (ConfigGetConfig(NIN_CFG_LED))
 		set32(HW_GPIO_OUT, GPIO_SLOT_LED);	//turn on drive light
@@ -65,7 +132,18 @@ DRESULT disk_read_usb(BYTE drv, BYTE *buff, DWORD sector, BYTE count)
 	return RES_OK;
 }
 
-DRESULT disk_write_usb(BYTE drv, const BYTE *buff, DWORD sector, BYTE count)
+
+
+/*-----------------------------------------------------------------------*/
+/* Write Sector(s) [USB storage]                                         */
+/*-----------------------------------------------------------------------*/
+
+#if _USE_WRITE
+DRESULT disk_write_usb (
+	BYTE pdrv,			/* Physical drive nmuber to identify the drive */
+	const BYTE *buff,	/* Data to be written */
+	DWORD sector,		/* Sector address in LBA */
+	UINT count			/* Number of sectors to write */
 {
 	if(USBStorage_WriteSectors(sector, count, buff) != 1)
 	{
@@ -75,27 +153,33 @@ DRESULT disk_write_usb(BYTE drv, const BYTE *buff, DWORD sector, BYTE count)
 
 	return RES_OK;
 }
+#endif
 
-DSTATUS disk_initialize( BYTE drv )
-{
-	return RES_OK;
-}
 
-DSTATUS disk_status( BYTE drv )
-{
-	return RES_OK;
-}
+/*-----------------------------------------------------------------------*/
+/* Miscellaneous Functions                                               */
+/*-----------------------------------------------------------------------*/
 
-DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff)
+#if _USE_IOCTL
+DRESULT disk_ioctl (
+	BYTE pdrv,		/* Physical drive nmuber (0..) */
+	BYTE cmd,		/* Control code */
+	void *buff		/* Buffer to send/receive control data */
+)
 {
 	if(cmd == GET_SECTOR_SIZE)
 		*(WORD*)buff = s_size;
 
 	return RES_OK;
 }
+#endif
 
+
+
+// Get the current system time as a FAT timestamp.
 DWORD get_fattime(void)
 {
+	// TODO: Implement this for Nintendont.
 	//rtc_time tm;
 	//DWORD ret;
 
@@ -111,6 +195,12 @@ DWORD get_fattime(void)
 	
 	return 0;
 }
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Nintendont: Device type selection.                                    */
+/*-----------------------------------------------------------------------*/
 
 DiskReadFunc disk_read;
 DiskWriteFunc disk_write;

kernel/diskio.h
@@ -1,23 +1,20 @@
-/*-----------------------------------------------------------------------
-/  Low level disk interface modlue include file  R0.07   (C)ChaN, 2009
-/-----------------------------------------------------------------------
-/ FatFs module is an open source project to implement FAT file system to small
-/ embedded systems. It is opened for education, research and development under
-/ license policy of following trems.
-/
-/  Copyright (C) 2009, ChaN, all right reserved.
-/
-/ * The FatFs module is a free software and there is no warranty.
-/ * You can use, modify and/or redistribute it for personal, non-profit or
-/   commercial use without any restriction under your responsibility.
-/ * Redistributions of source code must retain the above copyright notice.
-/
-/----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------/
+/  Low level disk interface modlue include file   (C)ChaN, 2014          /
+/-----------------------------------------------------------------------*/
 // original source: http://elm-chan.org/fsw/ff/00index_e.html
 
-#ifndef _DISKIO
+#ifndef _DISKIO_DEFINED
+#define _DISKIO_DEFINED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define _USE_WRITE	1	/* 1: Enable disk_write function */
+#define _USE_IOCTL	1	/* 1: Enable disk_ioctl fucntion */
 
 #include "integer.h"
+
 
 /* Status of Disk Functions */
 typedef BYTE	DSTATUS;
@@ -35,24 +32,32 @@ typedef enum {
 /*---------------------------------------*/
 /* Prototypes for disk control functions */
 
-void SetDiskFunctions(DWORD);
+DSTATUS disk_initialize (BYTE pdrv);
+DSTATUS disk_status (BYTE pdrv);
+DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff);
+DWORD get_fattime(void);
 
-typedef DRESULT (*DiskReadFunc)(BYTE, BYTE*, DWORD, BYTE);
-typedef DRESULT (*DiskWriteFunc)(BYTE, const BYTE*, DWORD, BYTE);
-
+/* Nintendont: R/W functions are pointers in order to *
+ * allow support for both SD cards and USB storage.   *
+ * NOTE: This *could* be implemented by using the     *
+ * "Physical Drive Number" (pdrv) parameter.          */
+typedef DRESULT (*DiskReadFunc)(BYTE pdrv, BYTE* buff, DWORD sector, UINT count);
+typedef DRESULT (*DiskWriteFunc)(BYTE pdrv, const BYTE* buff, DWORD sector, UINT count);
 extern DiskReadFunc disk_read;
 extern DiskWriteFunc disk_write;
 
-DSTATUS disk_initialize(BYTE);
-DSTATUS disk_status(BYTE);
-DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff);
-DWORD get_fattime(void);
+/**
+ * Nintendont: Initialize disk drive functions.
+ * @param usb 1 for USB; 0 for SD.
+ */
+void SetDiskFunctions(DWORD usb);
 
 /* Disk Status Bits (DSTATUS) */
 
 #define STA_NOINIT		0x01	/* Drive not initialized */
 #define STA_NODISK		0x02	/* No medium in the drive */
 #define STA_PROTECT		0x04	/* Write protected */
+
 
 /* Command code for disk_ioctrl fucntion */
 
@@ -81,5 +86,8 @@ DWORD get_fattime(void);
 #define ATA_GET_MODEL		21	/* Get model name */
 #define ATA_GET_SN			22	/* Get serial number */
 
-#define _DISKIO
+#ifdef __cplusplus
+}
+#endif
+
 #endif

commit 93ee8fed1e9a0228a6c36f460d6c427cc8ac7944

[kernel] diskio.c: Added some missing '('s.
This fixes a regression from commit 11840497ee5b32c219fc6f1c2cbdf6d775adeb0f.
([kernel] diskio.c: Partially rebased on FatFS R0.11a.)
enter the commit message for your
changes. Lines starting

kernel/diskio.c
@@ -56,6 +56,7 @@ DRESULT disk_read_sd (
 	BYTE *buff,		/* Data buffer to store read data */
 	DWORD sector,	/* Sector address in LBA */
 	UINT count		/* Number of sectors to read */
+)
 {
 	s32 Retry=10;
 
@@ -114,6 +115,7 @@ DRESULT disk_read_usb (
 	BYTE *buff,		/* Data buffer to store read data */
 	DWORD sector,	/* Sector address in LBA */
 	UINT count		/* Number of sectors to read */
+)
 {
 	if (ConfigGetConfig(NIN_CFG_LED))
 		set32(HW_GPIO_OUT, GPIO_SLOT_LED);	//turn on drive light
@@ -144,6 +146,7 @@ DRESULT disk_write_usb (
 	const BYTE *buff,	/* Data to be written */
 	DWORD sector,		/* Sector address in LBA */
 	UINT count			/* Number of sectors to write */
+)
 {
 	if(USBStorage_WriteSectors(sector, count, buff) != 1)
 	{
@@ -192,7 +195,7 @@ DWORD get_fattime(void)
 	//	| ((DWORD)tm.tm_min << 5)
 	//	| ((DWORD)tm.tm_sec >> 1);
 	//return ret;
-	
+
 	return 0;
 }
 